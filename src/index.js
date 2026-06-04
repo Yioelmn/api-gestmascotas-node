8f880bc1829d77bea4b85c2f3b400f1df2591953
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-06-04 18:32:20 -0400

    añadimos ruta a index ou yea

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import 'dotenv/config'; // para cargar las variables de entorno desde el archivo
 import express from 'express';
 import cors from 'cors';
 import { pool } from './config/db.js'; // para levantar conexion y crear tablas
+import etiquetasRoutes from './routes/etiquetasRoutes.js'; // importamos las rutas de etiquetas
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// se añade esto para conectar con las rutas
+app.use('/api', etiquetasRoutes);
 
 // ruta para probar si funciona jej
 app.get('/', (req, res) => {
