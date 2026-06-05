ad7b041f0896326e416eb07f9c54b649d2fc0d9d
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-06-05 00:18:24 -0400

    creacion de mascotasController para manejo de endpoints jojo

---
+++ src/controllers/mascotasController.js
@@ -0,0 +1,81 @@
+import { mascotasService } from "../services/mascotasService";
+
+export const mascotasController = {
+    async obtenerTodas(req, res){
+        try{
+            const mascotas = await mascotasService.obtenerTodas();
+            res.json(mascotas);
+        }catch (e){
+            res.status(500).json({ error: 'Error al obtener las mascotas' });
+        }
+    },
+
+    async obtenerPorId(req, res){
+        try{
+            const { id } = req.params;
+            const mascota = await mascotasService.obtenerPorId(id);
+            if (!mascota){
+                return res.status(404).json({ error: 'Mascota no encontrada' });
+            }
+            res.json(mascota);
+        }catch (e){
+            res.status(500).json({ error: 'Error al obtener la mascota'});
+        }
+    },
+
+    async crear (req, res){
+        try{
+            const{nombre, especie, raza, sexo, edad} = req.body;
+            //validacion de los campos obligatorios
+            if (!nombre || !especie || !raza || !sexo || edad === undefined){
+                return res.status(400).json({ error: 'Porfavor llenar campos obligatorios (nombre, especie, raza, sexo, edad'});
+            }
+
+            const nuevaMascota = await mascotasService.crear(req.body);
+            res.status(201).json(nuevaMascota);
+        }catch(e){
+            res.status(500).json({ error: 'Error al registrar la mascota'});
+        }
+    },
+
+    async actualizar(req, res){
+        try{
+            const {id} = req.params;
+            const mascotaActualizada = await mascotasService.actualizar(id, req.body);
+            if(!mascotaActualizada){
+                return res.status(404).json({ error: 'Mascota no encontrada' });
+            }
+            res.json(mascotaActualizada);
+        }catch(e){
+            res.status(500).json({ error: 'Error al actualizar la mascota' });
+        }
+    },
+
+    async eliminar(req, res){
+        try{
+            const {id} = req.params;
+            const eliminado = await mascotasService.eliminar(id);
+            if (!eliminado){
+                return res.status(404).json({ error: 'Mascota no encontrada' });
+            }
+            res.json({ message: 'Mascota eliminada exitosamente' });
+        }catch (e){
+            res.status(500).json({ error: 'Error al eliminar la mascota' });
+        }
+    },
+
+    // encpoint para vincular etiqueta a mascota
+    async vincularEtiqueta(req, res){
+        try{
+            const {id} = req.params;
+            const {etiqueta_id} = req.body;
+            if(!etiqueta_id){
+                return res.status(400).json({ error: 'etiqueta_id es requerido en el body' });
+            }
+            await mascotasService.agregarEtiqueta(id, etiqueta_id);
+            res.json({ message: 'Etiqueta vinculada correctamente' });
+        }catch (e){
+            res.status(500).json({ error: 'Error al vincular etiqueta' });
+        }
+    }
+};
