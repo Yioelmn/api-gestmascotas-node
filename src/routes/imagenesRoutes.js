ce0720d40a293e18e71467f86e7d202445187d46
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-06-05 01:47:15 -0400

    configuradas las rutas de imagenes

---
+++ src/routes/imagenesRoutes.js
@@ -0,0 +1,9 @@
+import {Router} from 'express';
+import { imagenesController } from '..controllers/imagenesController.js';
+
+const router = Router();
+
+router.post('/imagenes', imagenesController.crear);
+router.delete('/imagenes/:id', imagenesController.eliminar);
+
+export default router;
