cce2efed08e84f9972afa61bb79b7e574872e112
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-06-05 00:25:58 -0400

    rutas para endpoints de mascotas

---
+++ src/routes/mascotasRoutes.js
@@ -0,0 +1,13 @@
+import {Router} from 'express';
+import { mascotascontroller } from '../controllers/mascotasController.js';
+
+const router = Router();
+
+router.get('/mascotas', mascotasController.obtenerTodas);
+router.get('/mascotas/:id', mascotasController.obtenerPorId);
+router.post('/mascotas', mascotasController.crear);
+router.put('/mascotas/:id', mascotasController.actualizar);
+router.delete('/mascotas/:id', mascotasController.eliminar);
+router.post('/mascotas/:id/etiquetas', mascotasController.vincularEtiqueta)
+
+export default router;
