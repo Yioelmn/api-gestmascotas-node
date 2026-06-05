2927b133a256f249c22fa5efad206fb09f4ef83b
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-06-05 00:30:12 -0400

    ruta añadida al index, se me olvido hacerlo en el commit anterior

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import { pool } from './config/db.js'; // para levantar conexion y crear tablas
 import etiquetasRoutes from './routes/etiquetasRoutes.js'; // importamos las rutas de etiquetas
+import mascotasRoutes from '.routes/mascotasaRoutes.js'; // importamos las rutas de mascotas
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,7 @@ app.use(express.json());
 
 // se añade esto para conectar con las rutas
 app.use('/api', etiquetasRoutes);
+app.use('/api', mascotasRoutes);
 
 // ruta para probar si funciona jej
 app.get('/', (req, res) => {
