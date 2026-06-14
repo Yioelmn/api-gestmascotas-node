fd006f2bcb70c02eb514b5a7616fac9fc6309870
Yioelmn <jo.calfuqueo@duocuc.cl>
2026-06-14 17:17:12 -0400

    arreglando rutas por cuello de botella

--- src/routes/mascotasRoutes.js
+++ src/routes/mascotasRoutes.js
@@ -1,13 +1,13 @@
 import {Router} from 'express';
 import { mascotasController } from '../controllers/mascotasController.js';
-import { verificarToken } from '../middleware/authMiddleware.js'
+// import { verificarToken } from '../middleware/authMiddleware.js' <- despues lo usamos
 
 const router = Router();
 
 router.get('/mascotas', mascotasController.obtenerTodas);
 router.get('/mascotas/buscar', mascotasController.obtenerPorNombre);
 router.get('/mascotas/:id', mascotasController.obtenerPorId);
-router.post('/mascotas', verificarToken, mascotasController.crear);
+router.post('/mascotas', mascotasController.crear);
 router.put('/mascotas/:id', mascotasController.actualizar);
 router.delete('/mascotas/:id', mascotasController.eliminar);
 router.post('/mascotas/:id/etiquetas', mascotasController.vincularEtiqueta)
